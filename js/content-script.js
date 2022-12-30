98c651351c6fadf6f068fe8925edb20016cb76d9
YuuValue <120377877+yuuvalue@users.noreply.github.com>
2022-12-30 10:23:34 +0900

    Add plus/minus button processing
    We needed the ability to increase or decrease the number in a text box.
    Therefore, add a process to increase or decrease the numerical value by 0.1.
    Also, make it so that it reacts the entire time the button is pressed.

--- js/content-script.js
+++ js/content-script.js
@@ -1,6 +1,54 @@
 $(function () {
-    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" value="-"><span class="x_str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.01"><input type="button" class="tool" value="+"></div>');
-    $("#speed-num").on("keyup", function(event) {
+    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" id="minus-btn" value="-"><span class="x_str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.01"><input type="button" class="tool" id="plus-btn" value="+"></div>');
+    $("#speed-num").on("keyup", function (event) {
         event.stopPropagation();
+    });
+
+    function mouse_push_hold_minus() {
+        if (Number($("#speed-num").val()) >= 0.2) {
+            $("#speed-num").val(BigNumber(Number($("#speed-num").val())).minus(BigNumber(0.1)));
+        }
+        if (pushing_flag_minus == 1) {
+            setTimeout(mouse_push_hold_minus, 100);
+        }
+    };
+
+    function mouse_push_hold_plus() {
+        if (Number($("#speed-num").val()) <= 9.9) {
+            $("#speed-num").val(BigNumber(Number($("#speed-num").val())).plus(BigNumber(0.1)));
+        }
+        if (pushing_flag_plus == 1) {
+            setTimeout(mouse_push_hold_plus, 100);
+        }
+    };
+
+    $("#minus-btn").mousedown(function () {
+        pushing_flag_minus = 1;
+        setTimeout(mouse_push_hold_minus, 120);
+        return false;
+    }).mouseup(function () {
+        pushing_flag_minus = 0;
+        clearTimeout(mouse_push_hold_minus);
+    }).mouseleave(function () {
+        pushing_flag_minus = 2;
+        clearTimeout(mouse_push_hold_minus);
+    }).mouseover(function () {
+        pushing_flag_minus = 0;
+        clearTimeout(mouse_push_hold_minus);
+    });
+
+    $("#plus-btn").mousedown(function () {
+        pushing_flag_plus = 1;
+        setTimeout(mouse_push_hold_plus, 120);
+        return false;
+    }).mouseup(function () {
+        pushing_flag_plus = 0;
+        clearTimeout(mouse_push_hold_plus);
+    }).mouseleave(function () {
+        pushing_flag_plus = 2;
+        clearTimeout(mouse_push_hold_plus);
+    }).mouseover(function () {
+        pushing_flag_plus = 0;
+        clearTimeout(mouse_push_hold_plus);
     });
 });
