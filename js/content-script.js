ca442b84a9c42c7bd77e1674bf27667113901eec
YuuValue <120377877+yuuvalue@users.noreply.github.com>
2022-12-31 08:26:22 +0900

    Add response to speed changes
    It was difficult to tell when the speed was changed.
    So, add a blinking background animation.

--- js/content-script.js
+++ js/content-script.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" id="minus-btn" value="-"><span class="x_str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.01"><input type="button" class="tool" id="plus-btn" value="+"></div>');
+    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" id="minus-btn" value="-"><span id="x-str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.01"><input type="button" class="tool" id="plus-btn" value="+"></div>');
     $("#speed-num").on("keyup", function (event) {
         event.stopPropagation();
     });
@@ -27,22 +27,28 @@ $(function () {
     function changePlaybackRate() {
         document.getElementById("book-player").playbackRate = Number($("#speed-num").val());
         document.getElementById("book-player").defaultPlaybackRate = Number($("#speed-num").val());
+        $("#speed-num").addClass("speed-num-changed");
+        $("#x-str").addClass("speed-num-changed");
+        setTimeout(() => {
+            $("#speed-num").removeClass("speed-num-changed");
+            $("#x-str").removeClass("speed-num-changed");
+        }, 600);
     }
 
     function mouse_not_push() {
         pushing_flag = 0;
-        setTimeout(changePlaybackRate(), 125);
         if (push_count >= 1) {
+            setTimeout(changePlaybackRate(), 125);
+            push_count = 0;
             clearTimeout(timer);
         }
     }
 
     let timer;
-    let push_count;
+    let push_count = 0;
 
     $("#minus-btn").mousedown(function () {
         pushing_flag = 1;
-        push_count = 0;
         timer = setTimeout(mouse_push_hold_minus, 120);
         return false;
     }).mouseup(function () {
@@ -55,7 +61,6 @@ $(function () {
 
     $("#plus-btn").mousedown(function () {
         pushing_flag = 1;
-        push_count = 0;
         timer = setTimeout(mouse_push_hold_plus, 120);
         return false;
     }).mouseup(function () {
@@ -66,7 +71,7 @@ $(function () {
         mouse_not_push();
     });
 
-    $("#speed-num").change(function () {
+    $("#speed-num").on("change", function () {
         changePlaybackRate();
     });
 });
