e2b44ff7f516654dd329c1a6cc170cf3bb185bba
YuuValue <120377877+yuuvalue@users.noreply.github.com>
2022-12-30 20:20:47 +0900

    Fix the button response process
    setTimeout was not canceled when mouse was released.
    Therefore, the argument of the clearTimeout function was corrected.

--- js/content-script.js
+++ js/content-script.js
@@ -7,48 +7,56 @@ $(function () {
     function mouse_push_hold_minus() {
         if (Number($("#speed-num").val()) >= 0.2) {
             $("#speed-num").val(BigNumber(Number($("#speed-num").val())).minus(BigNumber(0.1)));
+            push_count += 1;
         }
-        if (pushing_flag_minus == 1) {
-            setTimeout(mouse_push_hold_minus, 100);
+        if (pushing_flag == 1) {
+            timer = setTimeout(mouse_push_hold_minus, 100);
         }
     };
 
     function mouse_push_hold_plus() {
         if (Number($("#speed-num").val()) <= 9.9) {
             $("#speed-num").val(BigNumber(Number($("#speed-num").val())).plus(BigNumber(0.1)));
+            push_count += 1;
         }
-        if (pushing_flag_plus == 1) {
-            setTimeout(mouse_push_hold_plus, 100);
+        if (pushing_flag == 1) {
+            timer = setTimeout(mouse_push_hold_plus, 100);
         }
     };
 
+    function mouse_not_push() {
+        pushing_flag = 0;
+        if (push_count >= 1) {
+            clearTimeout(timer);
+        }
+    }
+
+    let timer;
+    let push_count;
+
     $("#minus-btn").mousedown(function () {
-        pushing_flag_minus = 1;
-        setTimeout(mouse_push_hold_minus, 120);
+        pushing_flag = 1;
+        push_count = 0;
+        timer = setTimeout(mouse_push_hold_minus, 120);
         return false;
     }).mouseup(function () {
-        pushing_flag_minus = 0;
-        clearTimeout(mouse_push_hold_minus);
+        mouse_not_push();
     }).mouseleave(function () {
-        pushing_flag_minus = 2;
-        clearTimeout(mouse_push_hold_minus);
+        mouse_not_push();
     }).mouseover(function () {
-        pushing_flag_minus = 0;
-        clearTimeout(mouse_push_hold_minus);
+        mouse_not_push();
     });
 
     $("#plus-btn").mousedown(function () {
-        pushing_flag_plus = 1;
-        setTimeout(mouse_push_hold_plus, 120);
+        pushing_flag = 1;
+        push_count = 0;
+        timer = setTimeout(mouse_push_hold_plus, 120);
         return false;
     }).mouseup(function () {
-        pushing_flag_plus = 0;
-        clearTimeout(mouse_push_hold_plus);
+        mouse_not_push();
     }).mouseleave(function () {
-        pushing_flag_plus = 2;
-        clearTimeout(mouse_push_hold_plus);
+        mouse_not_push();
     }).mouseover(function () {
-        pushing_flag_plus = 0;
-        clearTimeout(mouse_push_hold_plus);
+        mouse_not_push();
     });
 });
