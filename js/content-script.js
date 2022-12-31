6de783fbba2bfa31123769b1dc1bab568ef8c2f8
YuuValue <120377877+yuuvalue@users.noreply.github.com>
2022-12-31 10:20:23 +0900

    Change the minimum value of the text box
    Fixed an error when the playback speed is less than 0.06.

--- js/content-script.js
+++ js/content-script.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" id="minus-btn" value="-"><span id="x-str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.01"><input type="button" class="tool" id="plus-btn" value="+"></div>');
+    $("#book-player").after('<div class="speed-controller"><input type="button" class="tool" id="minus-btn" value="-"><span id="x-str">x</span><input type="number" id="speed-num" value="1" step="0.01" max="10" min="0.1"><input type="button" class="tool" id="plus-btn" value="+"></div>');
     $("#speed-num").on("keyup", function (event) {
         event.stopPropagation();
     });
